863022836ac0ee93e77a42ff035667eaec9901ef
wongtheo <huangsiyao@RRB.local>
2023-02-18 21:30:33 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/model/CallRecordDO.java
+++ src/main/java/com/tencent/wxcloudrun/model/CallRecordDO.java
@@ -19,7 +19,7 @@ public class CallRecordDO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     /**
      * id
      */
