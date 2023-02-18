6e57e8bf7dfb1f9b83c161af1529b5c86cbbe6f7
wongtheo <huangsiyao@RRB.local>
2023-02-18 14:31:25 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/controller/DallEController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/DallEController.java
@@ -1,15 +1,17 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.param.MsgParam;
 import com.tencent.wxcloudrun.service.DallEService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/dalle")
@@ -20,7 +22,8 @@ public class DallEController {
     private DallEService dallEService;
 
     @PostMapping("/handleMsgs")
-    public ApiResponse handleMsg(@RequestBody MsgParam param){
+    public ApiResponse handleMsg(HttpServletRequest request, MsgParam param){
+        log.info("request={}", JSON.toJSONString(request, SerializerFeature.IgnoreErrorGetter));
         log.info("param={}", param);
         dallEService.handleMsg(param);
         return ApiResponse.ok();
