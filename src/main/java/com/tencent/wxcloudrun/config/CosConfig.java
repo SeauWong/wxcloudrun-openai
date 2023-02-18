8240b75d9d5760b4b9de1b378aab8b1ecec46164
wongtheo <huangsiyao@RRB.local>
2023-02-19 02:08:54 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/config/CosConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/CosConfig.java
@@ -6,11 +6,13 @@ import com.qcloud.cos.auth.BasicCOSCredentials;
 import com.qcloud.cos.auth.COSCredentials;
 import com.qcloud.cos.http.HttpProtocol;
 import com.qcloud.cos.region.Region;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class CosConfig {
 
     @Value("${cos.secretId:0}")
@@ -30,6 +32,7 @@ public class CosConfig {
         //用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
 
+        log.info("secreteId={}, secreteKey={}", secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参见 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
         ClientConfig clientConfig = new ClientConfig(new Region(region));
