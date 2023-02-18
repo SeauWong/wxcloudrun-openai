33c625a5dcd578b269e87f70865f4fff4b9f0a6a
wongtheo <huangsiyao@RRB.local>
2023-02-18 22:02:47 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/controller/DallEController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/DallEController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.tencent.wxcloudrun.param.MsgParam;
 import com.tencent.wxcloudrun.service.DallEService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,8 @@ public class DallEController {
     public String handleMsg(@RequestBody String body) {
         log.info("body={}", body);
         MsgParam param = JSON.parseObject(body, MsgParam.class);
-        if (!param.getContent().startsWith("作画")) {
+        if (StringUtils.isBlank(param.getContent())
+                || !param.getContent().startsWith("作画")) {
             return null;
         }
         if (!"text".equals(param.getMsgType())) {
