106e69f89ef20f31f56713c303958da6713836ce
wongtheo <huangsiyao@RRB.local>
2023-02-19 02:32:57 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/config/CosConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/CosConfig.java
@@ -17,11 +17,11 @@ import java.nio.file.LinkOption;
 @Slf4j
 public class CosConfig {
 
-    @Value("${cos.secretId:0}")
-    private String secretId;
+    @Value("${cos.sid}")
+    private String sid;
 
-    @Value("${cos.secretKey:0}")
-    private String secretKey;
+    @Value("${cos.skey}")
+    private String skey;
 
     @Value("${cos.region:0}")
     private String region;
@@ -30,11 +30,11 @@ public class CosConfig {
     public COSClient cosClient() {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
+        log.info("sid={}, skey={}", sid, skey);
 
         //用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+        COSCredentials cred = new BasicCOSCredentials(sid, skey);
 
-        log.info("secreteId={}, secreteKey={}", secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参见 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
         ClientConfig clientConfig = new ClientConfig(new Region(region));
