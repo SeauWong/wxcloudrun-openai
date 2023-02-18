f65c973b646e6e7db6d5f86c3a07955336660603
wongtheo <huangsiyao@RRB.local>
2023-02-18 21:14:30 +0800

    update

--- src/main/java/com/tencent/wxcloudrun/repository/CallRecordRepository.java
+++ src/main/java/com/tencent/wxcloudrun/repository/CallRecordRepository.java
@@ -2,14 +2,12 @@ package com.tencent.wxcloudrun.repository;
 
 import com.tencent.wxcloudrun.model.CallRecordDO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @description 会话记录
  * @author WongTheo
  * @date 2023-02-18
  */
-@Repository
 public interface CallRecordRepository extends JpaRepository<CallRecordDO,Long> {
 
 
